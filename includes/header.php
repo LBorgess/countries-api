a8216076849382efc841f843968b9820f88da667
LBorgess <lucas_rancer@hotmail.com>
2023-12-24 16:42:15 -0300

    Criação do header, footer e uso do bootstrap

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<?php defined('CONTROL') or die('Acesso negado.'); ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./includes/css/bootstrap.min.css">
+    <title>COUNTRIES - API</title>
+</head>
+
+<body>
