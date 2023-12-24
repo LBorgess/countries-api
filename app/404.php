31728a052938a6995675e0c70518c58eaccced36
LBorgess <lucas_rancer@hotmail.com>
2023-12-24 19:49:32 -0300

    Criação da página de erro 404

---
+++ app/404.php
@@ -0,0 +1,12 @@
+<?php defined('CONTROL') or die('Acesso negado.'); ?>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col text-center">
+            <div class="alert alert-danger" role="alert">
+                <h4>ERRO 404</h4>
+                <p>Recurso não encontrado.</p>
+            </div>
+        </div>
+    </div>
+</div>
