ebc7e571059a18307e8fa13507987ce78464fb97
LBorgess <lucas_rancer@hotmail.com>
2023-12-24 16:43:35 -0300

    Criação dos arquivos de rota, erro e home

---
+++ app/home.php
@@ -0,0 +1,9 @@
+<?php defined('CONTROL') or die('Acesso negado.'); ?>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col text-center">
+            <h3>Vamos consumir uma API com PHP Puro</h3>
+        </div>
+    </div>
+</div>
