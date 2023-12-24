9dbaf723bed5c232a317bea3473e15217e072d64
LBorgess <lucas_rancer@hotmail.com>
2023-12-24 16:43:11 -0300

    Mudança dos nomes das rotas

--- index.php
+++ index.php
@@ -3,5 +3,26 @@
 // Controle de fluxo da aplicação
 define('CONTROL', true);
 
-// Incluir as rotas / arquivos
-$routes = require_once('app/routes.php');
+// Incluir arquivos
+$routes = require_once('includes/routes.php');
+
+// Definir rota
+$route = $_GET['route'] ?? 'home';
+
+if (!in_array($route, $routes)) {
+    $route = '404';
+}
+
+// Fluxo das rotas
+switch ($route) {
+    case 'home':
+        require_once 'includes/header.php';
+        require_once 'app/home.php';
+        require_once 'includes/footer.php';
+        break;
+    case '404':
+        require_once 'includes/header.php';
+        require_once 'app/404.php';
+        require_once 'includes/footer.php';
+        break;
+}
