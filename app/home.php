c539583c921ed50ed9919e83f6783cf1201e14bc
LBorgess <lucas_rancer@hotmail.com>
2023-12-24 22:14:49 -0300

    Criação da seleção de países

--- app/home.php
+++ app/home.php
@@ -1,9 +1,42 @@
-<?php defined('CONTROL') or die('Acesso negado.'); ?>
+<?php
+defined('CONTROL') or die('Acesso negado.');
+
+$api = new ApiConsumer();
+
+// Obtém os dados dos países
+$countries = $api->get_all_countries();
+
+?>
 
 <div class="container mt-5">
+
     <div class="row">
         <div class="col text-center">
-            <h3>Vamos consumir uma API com PHP Puro</h3>
+            <h3>Países do Mundo</h3>
+            <hr>
         </div>
     </div>
+
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <p>Lista de países</p>
+            <select id="select_country" class="form-select">
+                <option value="">Selecione um país</option>
+                <?php foreach ($countries as $country) : ?>
+                    <option value="<?= $country ?>"><?= $country ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+    </div>
+
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        const select_country = document.querySelector('#select_country');
+        select_country.addEventListener('change', () => {
+            const country = select_country.value;
+            window.location.href = `?route=country&country_name=${country}`;
+        })
+    })
+</script>
