132dc3b760efd51a9f714c0f703c5d368b1c0620
LBorgess <lucas_rancer@hotmail.com>
2023-12-24 22:19:44 -0300

    Titulo da página

--- includes/header.php
+++ includes/header.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./includes/css/bootstrap.min.css">
-    <title>COUNTRIES - API</title>
+    <title>Countries - API</title>
 </head>
 
 <body>
